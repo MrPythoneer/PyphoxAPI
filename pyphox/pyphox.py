3c37c0d06e8b1196d179eec8bcf3f17626371af8
MrPythoneer <malikov.vp.dvfu@gmail.com>
2022-07-14 21:07:01 +1000

    Pyphox class written

---
+++ pyphox/pyphox.py
@@ -0,0 +1,98 @@
+import json
+from typing import Any
+from urllib.request import urlopen
+
+from .sensortypes import SensorType
+from .sensors import Sensor, VSensor, XYZSensor
+
+
+class Pyphox:
+    __slots__ = ('address', 'query', 'sensors', 'sensors_data')
+
+    address: str
+    query: str
+    sensors: list[str]
+    sensors_data: set[str, float]
+
+    def __init__(self, address: str, sensors: list[str]):
+        self.address = address
+        self.sensors = sensors
+        self.query = ""
+        self.sensors_data = []
+
+    def register_sensor(self, sensor: SensorType) -> Sensor:
+        ("""Returns XYZSensor or VSensor representing a sensor in the experiment."""
+         """Since VSensor has only one variable, it's automatically going to be fetched"""
+         """with Fetch()"""
+         """Since XYZSensor has several variables, none will be fetched with Update()."""
+         """In order to fetch data from the sensor, IncludeX, IncludeY or IncludeZ should"""
+         """be called.""")
+
+        if not self.has_sensor(sensor):
+            return None
+
+        prefix = sensor.prefix()
+        stype = sensor.value.type
+        if stype == 'XYZ':
+            return XYZSensor(self, prefix)
+
+        elif stype == 'V':
+            self.query += prefix + "&"
+            return VSensor(self, prefix)
+
+        else:
+            return None
+
+    def has_sensor(self, sensor: SensorType) -> bool:
+        """Returns true if the experiment uses the given sensor type, otheriwse, false will be returned"""
+        return sensor.value.name in self.sensors
+
+    def execute(self, command: str) -> dict[str, Any]:
+        """Executes some remote command on the host. Returns the JSON-like result of the command"""
+        resp = urlopen(self.address + command)
+        data = json.load(resp)
+        resp.close()
+
+        return data
+
+    def fetch(self) -> None:
+        """Requests the remote host for the latest data. The data will be saved to the SensorsData field"""
+        res = self.execute("/get?" + self.query)
+
+        data = {}
+        for k, v in res["buffer"].items():
+            data[k] = v["buffer"][0]
+
+        self.sensors_data = data
+
+    def start(self) -> bool:
+        """Starts measuring. By default, Fetch() is called automatically"""
+        res = self.execute("/control?cmd=start")
+        self.fetch()
+        return res["result"]
+
+    def stop(self) -> bool:
+        """Stops measuring"""
+        res = self.execute("/control?cmd=stop")
+        return res["result"]
+
+    def clear(self) -> bool:
+        """Clears the experiment buffer"""
+        res = self.execute("/control?cmd=clear")
+        return res["result"]
+
+
+def connect(address: str) -> Pyphox:
+    """Connects to the remote experiment at the given address"""
+
+    address = "http://" + address
+    config_resp = urlopen(address + "/config")
+
+    sensors = []
+    for inp in json.load(config_resp)['inputs']:
+        sensor = inp['source']
+        sensors.append(sensor)
+
+    config_resp.close()
+
+    return Pyphox(address, sensors)
--- pyphox/sensors.py
+++ pyphox/sensors.py
@@ -1,20 +1,23 @@
+from typing import Any
+
+
 class Sensor:
     """Represents a Phyphox sensor"""
 
     __slots__ = ('prefix', 'phyphox')
     prefix: str
-    phyphox: Phyphox
+    phyphox: Any
 
-    def __init__(self, prefix: str, phyphox: Phyphox) -> None:
+    def __init__(self, phyphox: Any, prefix: str) -> None:
         self.prefix = prefix
         self.phyphox = phyphox
 
     def get(self, var: str) -> float:
-        """Returns the 'var' value of the sensor from phyphox SensorsData"""
-        return self.phyphox.SensorsData[self.prefix+var]
+        """Returns the 'var' value of the sensor from phyphox sensors_data"""
+        return self.phyphox.sensors_data[self.prefix+var]
 
     def get_time(self) -> float:
-        """Returns the Time value of the sensor from phyphox SensorsData"""
+        """Returns the Time value of the sensor from phyphox sensors_data"""
         return self.get('_time')
 
     def include_time(self) -> None:
@@ -26,15 +29,15 @@ class XYZSensor(Sensor):
     """Represents a sensor with X, Y, and Z variables"""
 
     def get_x(self) -> float:
-        """Returns the X value of the sensor from phyphox SensorsData"""
+        """Returns the X value of the sensor from phyphox sensors_data"""
         return self.get('X')
 
     def get_y(self) -> float:
-        """Returns the Y value of the sensor from phyphox SensorsData"""
+        """Returns the Y value of the sensor from phyphox sensors_data"""
         return self.get('Y')
 
     def get_z(self) -> float:
-        """Returns the Z value of the sensor from phyphox SensorsData"""
+        """Returns the Z value of the sensor from phyphox sensors_data"""
         return self.get('Z')
 
     def include_x(self) -> None:
@@ -60,5 +63,5 @@ class VSensor(Sensor):
     """Represents a single-value sensor"""
 
     def get_value(self) -> float:
-        """Returns the value of the sensor from phyphox SensorsData"""
-        return self.phyphox.SensorsData[self.prefix]
+        """Returns the value of the sensor from phyphox sensors_data"""
+        return self.phyphox.sensors_data[self.prefix]
